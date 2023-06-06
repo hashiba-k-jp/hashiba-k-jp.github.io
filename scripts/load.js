2a2619dec87903b65728c1c59bdb3013f50aa1c4
hashiba-k-jp <17132@hakodate.kosen-ac.jp>
2023-06-06 18:37:32 +0900

    add animation

--- scripts/load.js
+++ scripts/load.js
@@ -6,6 +6,8 @@ document.addEventListener('DOMContentLoaded',
 window.addEventListener('load',
   function(){
     console.log('All contens have been loaded');
-    document.querySelector("#loading").style.cssText = 'display:none !important';
+    // document.querySelector("#loading").style.cssText = 'display:none !important';
+    document.querySelector("#loading").classList.add("-visible");
+
   }
 );
