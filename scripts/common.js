00d37ba799f361d85fe8c7f9647b9ebb3c9e4f25
hashiba-k-jp <17132@hakodate.kosen-ac.jp>
2023-06-19 16:52:15 +0900

    add nav-border

--- scripts/common.js
+++ scripts/common.js
@@ -9,6 +9,7 @@ function nav(pageName){
             <a class=\"navbar-brand p-2 m-0 d-flex align-items-center\" href=\"/\"><img src=\"/images/icons/icon.jpg\" class=\"border border-light border-1 rounded-circle d-inline-block align-text-top\" height=\"36\"></a>\
             <a class=\"navbar-brand o-font-head p-0\">" + pageName +"</a>\
         </div>\
+        <div class=\"nav-border\"></div>\
     "
 
     body.insertBefore(nav, main);
