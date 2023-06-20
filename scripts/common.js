a306946f236e5b35b75e1ab67bd64b47f82e4c52
hashiba-k-jp <17132@hakodate.kosen-ac.jp>
2023-06-20 15:07:11 +0900

    small change

--- scripts/common.js
+++ scripts/common.js
@@ -5,10 +5,10 @@ function nav(pageName){
     nav = document.createElement("nav");
     nav.classList.add("navbar", "navbar-expand-md", "navbar-dark", "fixed-top", "bg-dark", "p-0");
     nav.innerHTML = "\
-        <div class=\"container-md\">\
-            <a class=\"navbar-brand p-2 m-0 d-flex align-items-center\" href=\"/\"><img src=\"/images/icons/icon.jpg\" class=\"border border-light border-1 rounded-circle d-inline-block align-text-top\" height=\"36\"></a>\
-            <a class=\"navbar-brand o-font-head p-0\">" + pageName +"</a>\
-        </div>\
+            <div class=\"container-fluid\">\
+                <a class=\"navbar-brand p-2 m-0 d-flex align-items-center\" href=\"/\"><img src=\"/images/icons/icon.jpg\" class=\"border border-light border-1 rounded-circle d-inline-block align-text-top\" height=\"36\"></a>\
+                <a class=\"navbar-brand o-font-head p-0\">" + pageName +"</a>\
+            </div>\
         <!--<div class=\"nav-border\"></div>-->\
     "
 
